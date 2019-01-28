cfaf42ca98c726e3bf15da5a2169fe90a7d8386c
Christoffer Sundqvist <fiskmoz96@gmail.com>
2019-01-28 09:22:23 +0100

    Custom login work in progress

--- WebServer/WebServer/customauth.py
+++ WebServer/WebServer/customauth.py
@@ -1,8 +1,12 @@
 import requests
+from django.template import loader, Context
+from django.http import HttpResponse
 APIaddr = 'http://127.0.0.1:7000/'
 
 def login(request): 
     if request.method == "POST": 
         r = requests.get(APIaddr + 'auth/', )
     if request.method == "GET": 
-        
+        template = loader.get_template('login.html')
+        context = {}
+        return HttpResponse(template.render(context,request))
--- WebServer/WebServer/urls.py
+++ WebServer/WebServer/urls.py
@@ -19,8 +19,7 @@ from django.views.generic.base import TemplateView
 from . import customauth
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    patn('login/', customauth.login)
-    path('accounts/', include('django.contrib.auth.urls')), 
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('login/', customauth.login),
 ]
