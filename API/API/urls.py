b8fc8366878d6f47f8a07bfdb2c62a4409f10585
Joakim Djuvfeldt <jocke150@hotmail.com>
2019-02-18 14:45:17 +0100

    made it possible to add files

--- API/API/urls.py
+++ API/API/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('v1/', include('quickstart.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- API/quickstart/views.py
+++ API/quickstart/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from quickstart.auth import validate
 from quickstart.models import Quiz, QuizEntry
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.core.files.storage import FileSystemStorage
+from .serializer import FileSerializer
 
 
 class Home(APIView): 
@@ -15,7 +18,7 @@ class Home(APIView):
         usernames = [user.username for user in User.objects.all()]
         return Response(usernames)
 
-class Quiz(APIView): 
+class QuizView(APIView): 
     def post(self, request, format=None): 
         # if request.user.is_anonymous: 
         #     return Response(data='You are not authenticated!', status=status.HTTP_400_BAD_REQUEST)
@@ -25,3 +28,19 @@ class Quiz(APIView):
         print(request.POST)
         print(request)
         return Response(data="SUCCESS! :D ", status = status.HTTP_200_OK)
+
+class FileView(APIView):
+    parser_classes = (MultiPartParser, FormParser )
+    def post(self, request, *args, **kwargs):
+        if request.user.is_anonymous:
+            return Response(data='You are not authenticated!', status=status.HTTP_400_BAD_REQUEST)
+        if not validate(request.user.username, request.user.password):
+            return Response(data='Not authorized', status=status.HTTP_401_UNAUTHORIZED)
+        file_serializer = FileSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
