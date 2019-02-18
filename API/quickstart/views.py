80940cd4d890d2c93366e48750a6bb92720753b3
Christoffer Sundqvist <fiskmoz96@gmail.com>
2019-02-18 11:04:55 +0100

    lots of API changes

--- API/quickstart/views.py
+++ API/quickstart/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import status
-from API.auth import validate
+from quickstart.auth import validate
 
 
 class Home(APIView): 
@@ -13,3 +13,11 @@ class Home(APIView):
             return Response(data='Not authorized', status=status.HTTP_401_UNAUTHORIZED)
         usernames = [user.username for user in User.objects.all()]
         return Response(usernames)
+
+class Quiz(APIView): 
+    def post(self, request, format=None): 
+        if request.user.is_anonymous: 
+            return Response(data='You are not authenticated!', status=status.HTTP_400_BAD_REQUEST)
+        if not validate(request.user.username, request.user.password):
+            return Response(data='Not authorized', status=status.HTTP_401_UNAUTHORIZED)
+        
