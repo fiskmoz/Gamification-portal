ea63f68b7363d60e66840852bf28e602a8e44f6b
Christoffer Sundqvist <fiskmoz96@gmail.com>
2019-02-19 10:00:32 +0100

    Serialisers for geting quizes

--- API/quickstart/serializer.py
+++ API/quickstart/serializer.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import File
+from .models import File, Quiz, QuizEntry
 
 class FileSerializer(serializers.ModelSerializer):
 
     class Meta():
         model = File
         fields = ('name', 'file')
+
+class QuizSerializer(serializers.ModelSerializer): 
+
+    class Meta():
+        model = Quiz
+        fields = ('__all__')
+
+class QuizEntrySerializer(serializers.ModelSerializer): 
+
+    class Meta():
+        model = QuizEntry
+        fields = ('__all__')
--- API/quickstart/views.py
+++ API/quickstart/views.py
@@ -7,7 +7,7 @@ from quickstart.models import Quiz, QuizEntry
 from django.utils import timezone
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.core.files.storage import FileSystemStorage
-from .serializer import FileSerializer
+from .serializer import FileSerializer, QuizSerializer, QuizEntrySerializer
 from .models import File
 
 
@@ -21,12 +21,20 @@ class Home(APIView):
         return Response(usernames)
 
 class QuizView(APIView): 
+    def get(self, request, format=None): 
+        # if request.user.is_anonymous: 
+        #     return Response(data='You are not authenticated!', status=status.HTTP_400_BAD_REQUEST)
+        # if not validate(request.user.username, request.user.password):
+        #     return Response(data='Not authorized', status=status.HTTP_401_UNAUTHORIZED)
+        quizes = Quiz.objects.all()
+        serializer = QuizSerializer(quizes, many = True)
+        return Response(serializer.data)
+
     def post(self, request, format=None): 
         # if request.user.is_anonymous: 
         #     return Response(data='You are not authenticated!', status=status.HTTP_400_BAD_REQUEST)
         # if not validate(request.user.username, request.user.password):
         #     return Response(data='Not authorized', status=status.HTTP_401_UNAUTHORIZED)
-        # quiz = Quiz()
         print(request.POST)
         counter = 3 
         name = request.POST.get("Quizname")
@@ -36,17 +44,14 @@ class QuizView(APIView):
             return
         quiz = Quiz(QuizName=name, QuizCreator="default", Date = timezone.now()) 
         quiz.save()
-        print("Creating a QUIZ!")
         myList = []
         length = int(len(request.POST))
         while counter < length:
             myList.append(request.POST.get(str(counter)))
             tempLen = int(len(myList))
-            print("Counter: " + str(counter) + "Appended: " + request.POST.get(str(counter)) + "TempLen: " + str(tempLen) + "lenght: " + str(length))
             if tempLen > 4 :
                 quizentry = QuizEntry(QuizID = quiz, Question = myList[0], AlternativeA = myList[1], AlternativeB = myList[2], AlternativeC = myList[3], Correct = myList[4])
                 myList.clear()
-                print("Creating QuizEntry")
                 quizentry.save()
             counter = counter+1
         return Response(data="SUCCESS! :D ", status = status.HTTP_200_OK)
