69f85d2483e3151ae7a08eb26339c7b5923af02f
Christoffer Sundqvist <fiskmoz96@gmail.com>
2019-02-26 10:08:26 +0100

    Fadein and changes to use jquery

--- WebServer/static/assets/js/Create.js
+++ WebServer/static/assets/js/Create.js
@@ -5,108 +5,96 @@ var repeats = 0;
 function validate(name)
 {
     missionType = name; 
-    // CHECK IF MISSION NAME IS UNIQUE
-    document.getElementById('alternatives').style.display = 'none';
+    // CHECK IF MISSION NAME IS UNIQUE¨
+    $('#alternatives').hide();
     switch(missionType)
     {
         case "quiz": 
-            document.getElementById('quiz_section1').classList.remove('hidden');
-            document.getElementById('id_headertext').innerHTML = "You have chosen a " + missionType;
+            $('#quiz_section1').fadeIn(200).removeClass('hidden');
             break;
         case "article":
-            document.getElementById("news_article").classList.remove('hidden');
-            document.getElementById('id_headertext').innerHTML = "You have chosen a " + missionType;
+            $('#news_article').fadeIn(200).removeClass('hidden');
         break; 
     }
+    $('#id_headertext').innerHTML = "You have chosen a " + missionType;
 }
 
 function quizstage1()
 {
     // CHECK MISSION NAME UNIQUE¨
-    document.getElementById('quiz_section1').classList.add('hidden');
-    document.getElementById('quiz_section2').classList.remove('hidden');
-
-    name = document.getElementById('id_missionname').value;
-    desc = document.getElementById('id_missiondescription').value;
-    repeats = document.getElementById('id_numberofquestions').value; 
-    myTableArray.push(name);
-    myTableArray.push(desc);
-    myTableArray.push(repeats);
-
-    if(repeats < 1 || repeats > 35)
-    {
-        console.log(repeats);
-        return;
-    }
-    document.getElementById('id_headertext').innerHTML = "You have chosen a " + missionType + " with " + repeats.toString() + " questions!";
-    document.getElementById('quizform').style.display = "table";
+    $('#quiz_section1').addClass('hidden');
+    $('#quiz_section2').fadeIn(200).removeClass('hidden');
+    myTableArray.push($('#id_missionname').value);
+    myTableArray.push($('#id_missiondescription').value);
+    $('#id_headertext').fadeOut(function (){ $(this).innerHTML = "You have chosen a " + missionType + " with " + repeats.toString() + " questions!";}).fadeIn();
+    $('#quizform').fadeIn(200).css('display','table');
 }
 
 function quizstage2()
 {
-    myTableArray.push(document.getElementById("id_quizquestion".value));
-    myTableArray.push(document.getElementById("id_aswereA").value);
-    myTableArray.push(document.getElementById("id_aswereB").value);
-    myTableArray.push(document.getElementById("id_aswereC").value);
-    myTableArray.push(document.getElementById("id_correctanswer").value);
+    myTableArray.push($('#id_quizquestion').value);
+    myTableArray.push($('#id_answereA').value);
+    myTableArray.push($('#id_answereB').value);
+    myTableArray.push($('#id_answereC').vlaue);
+    myTableArray.push($('#id_correctanswer').value);
 
-    document.getElementById("id_quizquestion").value = "";
-    document.getElementById("id_aswereA").value = "";
-    document.getElementById("id_aswereB").value = "";
-    document.getElementById("id_aswereC").value = "";
+    $('#id_quizquestion').value = "";
+    $('#id_answereA').value = "";
+    $('#id_answereB').value = "";
+    $('#id_answereC').value = "";
     $('input[name=correctanswer]').attr('checked',false);
 
-    repeats = repeats -1;
+    repeats = repeats +1;
     $(window).scrollTop(0);
-    if(repeats <= 0)
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+      });
+      
+      Toast.fire({
+        type: 'success',
+        title: 'Information valid!'
+      })
+      document.getElementById('id_headertext').innerHTML = "You have chosen a " + missionType + " with " + repeats.toString() + " questions!";
+}
+function quizexit()
+{
+    var myJson = {};
+    myJson['Quizname'] = myTableArray[0];
+    myJson['Description'] = myTableArray[1];
+    var i;
+    for(i=2; i<myTableArray.length;i++)
     {
-        var myJson = {};
-        myJson['Quizname'] = myTableArray[0];
-        myJson['Description'] = myTableArray[1];
-        myJson['Size'] = myTableArray[2];
-        var i;
-        for(i=3; i<myTableArray.length;i++)
-        {
-            myJson[i] = myTableArray[i];
-        }
-        var request = $.ajax({
-        url: "http://127.0.0.1:7000/v1/quiz/",
-        type: "POST",
-        data: myJson
-        });
-        
-        DisplayResults(request);
+        myJson[i] = myTableArray[i];
     }
-    else 
-    {
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000
-          });
-          
-          Toast.fire({
-            type: 'success',
-            title: 'Information valid!'
-          })
-          document.getElementById('id_headertext').innerHTML = repeats.toString() + " questions left!";
-    }
+    var request = $.ajax({
+    url: "http://127.0.0.1:7000/v1/quiz/",
+    type: "POST",
+    data: myJson
+    });
+    
+    DisplayResults(request);
 }
 
 function submitArticle()
 {
     var myJson = {};
-    myJson["ArticleTitle"] = document.getElementById('articleTitle').value;
-    myJson["ArticleDescription"] = document.getElementById('articleDesc').value;
+    myJson["ArticleTitle"] = $('#articleTitle').value; 
+    myJson["ArticleDescription"] = $('#articleDesc').value;
     myJson["ArticleQuiz"] = $('select#quizSelection option:selected').val();
-
+    $("#fileuploader").uploadFile({
+        url:"http://127.0.0.1:7000/v1/news/fileupload/",
+        fileName:"myfile"
+        });
     // alert(JSON.stringify(myJson));
     var request = $.ajax({
         url: "http://127.0.0.1:7000/v1/news/",
         type: "POST",
         data: myJson
         });
+
     DisplayResults(request);
 }
 
