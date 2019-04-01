fa9acf1f72344ec1da79a726dca34580fbdad9f2
Joakim Djuvfeldt <jocke150@hotmail.com>
2019-04-01 13:55:33 +0200

    Changed api-view of models

--- API/quickstart/models.py
+++ API/quickstart/models.py
@@ -8,6 +8,9 @@ class Quiz(models.Model):
     Description = models.CharField(max_length = 4000, default = "")
     date = models.CharField(max_length=100, default = timezone.now())
 
+    def __str__(self):
+        return str(self.QuizName)
+
 class QuizEntry(models.Model):
     QuizID = models.ForeignKey(Quiz, to_field='id', on_delete=models.CASCADE, default=None)
     Question = models.CharField(max_length=250)
@@ -16,13 +19,16 @@ class QuizEntry(models.Model):
     AlternativeC = models.CharField(max_length=250)
     Correct = models.CharField(max_length=250)
 
+    def __str__(self):
+        return "Question for: " + str(self.QuizID)
+
 class File(models.Model):
     id = models.AutoField(max_length=250, primary_key=True)
     name = models.CharField(max_length=250)
     file = models.FileField(upload_to="files/", unique=True, blank=False, null=False, default="UNDEFINED")
 
-    # def __str__(self):
-    #     return self.name + ": " + str(self.file)
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     id = models.AutoField(max_length=250, primary_key=True)
@@ -31,12 +37,21 @@ class Article(models.Model):
     description = models.CharField(max_length=4000)
     shortDescription = models.CharField(max_length= 300, default = "A description")
 
+    def __str__(self):
+        return str(self.title)
+
 class ArticleLink(models.Model):
     id = models.AutoField(max_length=250, primary_key=True)
     article = models.ForeignKey(Article, to_field="id", on_delete=models.CASCADE, default=None)   
     filePath = models.ForeignKey(File, to_field="file", on_delete=models.CASCADE, default=None)
 
+    def __str__(self):
+        return "File for: " + str(self.article)
+
 class QuizLink(models.Model):
     id = models.AutoField(max_length=250, primary_key=True)
     article = models.ForeignKey(Article, to_field="id", on_delete=models.CASCADE, default=None) 
     quiz = models.ForeignKey(Quiz, to_field="id", on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return str(self.quiz) +" --- "+ str(self.article)
