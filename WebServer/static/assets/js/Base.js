880dda78854e578a953f6631c5e8cfaef0de5a17
Christoffer Sundqvist <fiskmoz96@gmail.com>
2019-04-17 12:40:25 +0200

    Navbar changes

--- WebServer/static/assets/js/Base.js
+++ WebServer/static/assets/js/Base.js
@@ -21,13 +21,13 @@ function DisplayResults(request)
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-60px";
-  }
-  prevScrollpos = currentScrollPos;
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos  && $(window).scrollTop() == 0) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-60px";
+    }
+    prevScrollpos = currentScrollPos;
 }
 
 var DEBUG = false;
